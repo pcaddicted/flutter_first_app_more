f5098f5e4b947cf5c1d6f482fb1c116dac7c16fb
wenbo <437071107@qq.com>
2018-11-20 23:36:33 +0800

    listview简易版本

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,118 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+    final buildings = [
+      Building(BuildingType.theater, 'CineArts at the Empire', '85 W Portal Ave'),
+      Building(BuildingType.theater, 'The Castro Theater', '429 Castro St'),
+      Building(BuildingType.theater, 'Alamo Drafthouse Cinema', '2550 Mission St'),
+      Building(BuildingType.theater, 'Roxie Theater', '3117 16th St'),
+      Building(BuildingType.theater, 'United Artists Stonestown Twin', '501 Buckingham Way'),
+      Building(BuildingType.theater, 'AMC Metreon 16', '135 4th St #3000'),
+      Building(BuildingType.restaurant, 'K\'s Kitchen', '1923 Ocean Ave'),
+      Building(BuildingType.restaurant, 'Chaiya Thai Restaurant', '72 Claremont Blvd'),
+      Building(BuildingType.restaurant, 'La Ciccia', '291 30th St'),
+
+      // double ??€???
+      Building(BuildingType.theater, 'CineArts at the Empire', '85 W Portal Ave'),
+      Building(BuildingType.theater, 'The Castro Theater', '429 Castro St'),
+      Building(BuildingType.theater, 'Alamo Drafthouse Cinema', '2550 Mission St'),
+      Building(BuildingType.theater, 'Roxie Theater', '3117 16th St'),
+      Building(BuildingType.theater, 'United Artists Stonestown Twin', '501 Buckingham Way'),
+      Building(BuildingType.theater, 'AMC Metreon 16', '135 4th St #3000'),
+      Building(BuildingType.restaurant, 'K\'s Kitchen', '1923 Ocean Ave'),
+      Building(BuildingType.restaurant, 'Chaiya Thai Restaurant', '72 Claremont Blvd'),
+      Building(BuildingType.restaurant, 'La Ciccia', '291 30th St'),
+    ];
+    return MaterialApp(
+      title: 'ListView demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Buildings'),
+        ),
+        body: BuildingListView(buildings, (index) => debugPrint('item $index clicked'))
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+enum BuildingType { theater, restaurant }
 
+class Building {
+  final BuildingType type;
   final String title;
+  final String address;
 
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  Building(this.type, this.title, this.address);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+typedef OnItemClickListener = void Function(int position);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ItemView extends StatelessWidget {
+  final int position;
+  final Building building;
+  final OnItemClickListener listener;
+
+  ItemView(this.position, this.building, this.listener);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    final icon = Icon(
+        building.type == BuildingType.restaurant
+            ? Icons.restaurant
+            : Icons.theaters,
+        color: Colors.blue[500]);
+
+    final widget = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(16.0),
+          child: icon,
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(building.title,
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
+                  )),
+              Text(building.address)
+            ],
+          ),
+        )
+      ],
+    );
+
+    return InkWell(
+      onTap: () => listener(position),
+      child: widget
+    );
+  }
+}
+
+class BuildingListView extends StatelessWidget {
+  final List<Building> buildings;
+  final OnItemClickListener listener;
+
+  BuildingListView(this.buildings, this.listener);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: buildings.length,
+      itemBuilder: (context, index) {
+        return new ItemView(index, buildings[index], listener);
+      }
     );
   }
 }
